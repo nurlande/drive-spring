703defb9664b444a00542b20a443b01bb8e2b3c0
myrza <Gapusta97@gmail.com>
2021-04-10 17:41:41 +0600

    7th attempt (The Migrations.class file has been found but for some reason the APIs of my Flyway dependency (local) and in heroku doesn't match so I'm gonna use older API and see what will happen)

--- src/main/java/edu/myrza/todoapp/config/Migrations.java
+++ src/main/java/edu/myrza/todoapp/config/Migrations.java
@@ -4,14 +4,19 @@ import org.flywaydb.core.Flyway;
 
 public class Migrations {
     public static void main(String[] args) {
-        Flyway flyway = Flyway.configure()
-                            .dataSource(
-                                System.getenv("JDBC_DATABASE_URL"),
-                                System.getenv("JDBC_DATABASE_USERNAME"),
-                                System.getenv("JDBC_DATABASE_PASSWORD")
-                            )
-                            .load();
-
+//        Flyway flyway = Flyway.configure()
+//                            .dataSource(
+//                                System.getenv("JDBC_DATABASE_URL"),
+//                                System.getenv("JDBC_DATABASE_USERNAME"),
+//                                System.getenv("JDBC_DATABASE_PASSWORD")
+//                            )
+//                            .load();
+//
+//        flyway.migrate();
+        Flyway flyway = new Flyway();
+        flyway.setDataSource(System.getenv("JDBC_DATABASE_URL"),
+                System.getenv("JDBC_DATABASE_USERNAME"),
+                System.getenv("JDBC_DATABASE_PASSWORD"));
         flyway.migrate();
     }
 }
